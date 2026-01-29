c384b9d9bf3f6ff8741d2cd2e686e58aeec220cc
fantush113 <105595789+fantush113@users.noreply.github.com>
2026-01-29 09:11:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ def start(msg):
         telebot.types.InlineKeyboardButton("🌐 IP ADDRESS", callback_data="ip")
     )
     kb.add(
-        telebot.types.InlineKeyboardButton("👤 Owner", url="https://t.me/selectionsevabot")
+        telebot.types.InlineKeyboardButton("👤 Owner", url="https://t.me/ZynLookup")
     )
 
     if dp_exists:
